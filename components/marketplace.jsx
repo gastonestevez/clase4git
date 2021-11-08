7932b12e0e89b78cf8caa3a130057e6f69f8755f
Gastón Estévez <gaston@digitalhouse.edu.ar>
2021-11-08 20:58:25 -0300

    agrego componente de marketplace

---
+++ components/marketplace.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const marketplace = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+marketplace.propTypes = {
+
+}
+
+export default marketplace
