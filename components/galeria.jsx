2b102c340dd0611dd2f65c586694904bb2c83125
Gastón Estévez <gaston@digitalhouse.edu.ar>
2021-11-08 20:58:02 -0300

    agrego componente de galeria

---
+++ components/galeria.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const galeria = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+galeria.propTypes = {
+
+}
+
+export default galeria
