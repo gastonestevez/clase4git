50d2605fbda8bf4f26d9d06cb0db34b3c08c3d17
Gastón Estévez <gaston@digitalhouse.edu.ar>
2021-11-08 20:57:36 -0300

    agrego componente cart

---
+++ components/cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const cart = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+cart.propTypes = {
+
+}
+
+export default cart
